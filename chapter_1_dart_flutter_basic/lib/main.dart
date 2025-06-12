63a183b02f03b6645287e376c0c0f2eec9fe6270
stevechow <zhmnda@icloud.com>
2025-06-12 09:09:41 +0800

    update dart part

--- chapter_1_dart_flutter_basic/lib/main.dart
+++ chapter_1_dart_flutter_basic/lib/main.dart
@@ -40,22 +40,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text("hello",
+    return const Scaffold(
+      body: Center(
+        child: Text("hello world",
             style: TextStyle(
               fontSize: 24,
-              color: Colors.amber,
+              color: Colors.black,
             )),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
